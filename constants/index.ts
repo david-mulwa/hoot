72b70161baf751b2b4949a4b07a3531dda1b26b0
David Mulwa <108262061+david-mulwa@users.noreply.github.com>
2024-11-13 17:52:43 +0300

    the index page styling and introduction of the placeorderPageScreen to navigate with the servise and type /geographcal elements

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+
+
+export const services =[
+    {
+        id: 0,
+        image:'https://www.freepik.com/free-vector/delivery-service-illustrated-concept_7680251.htm#fromView=search&page=2&position=36&uuid=38fd33fc-b67a-4376-8365-6576b555d4a3',
+        name:'Delivery Man'
+    },
+    {
+        id: 1,
+        image:'https://www.freepik.com/free-vector/truck_3093274.htm#fromView=keyword&page=1&position=17&uuid=a0be5b51-8ed5-4533-b7e1-fe920e8f2f97',
+        name:'Truck'
+    },
+    {
+        id: 2,
+        image:'https://www.freepik.com/free-vector/plane-travelling-around-globe_4978302.htm#fromView=search&page=1&position=16&uuid=ed6e6123-0e4e-4a60-b13b-1a5c19180a09',
+        name:'Airplane'
+    },
+    {
+        id: 3,
+        image:'https://www.freepik.com/free-vector/hand-drawn-container-ship_20289239.htm#fromView=search&page=1&position=49&uuid=20339c2c-8616-469e-bf48-89a39bb382dd',
+        name:'Boat'
+    },
+]
